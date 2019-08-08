7817a750b630751d45a207978ae6b2cfdd6ad03b
Daniel Rocha <rocha.daniel52@gmail.com>
2019-08-08 16:39:41 -0300

    Creating Activity extension to show toast messages

---
+++ app/src/main/java/com/example/basearch/presentation/extensions/ActivityExtensions.kt
@@ -0,0 +1,8 @@
+package com.example.basearch.presentation.extensions
+
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
