6863ac129e00013580451ccbe291957253cfe707
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-03 10:24:38 -0300

    Renaming User class that represents API response

--- data/src/main/java/br/com/pebmed/data/remote/model/response/PullRequestResponse.kt
+++ data/src/main/java/br/com/pebmed/data/remote/model/response/PullRequestResponse.kt
@@ -6,7 +6,7 @@ import java.util.*
 data class PullRequestResponse(
     @SerializedName("html_url") val htmlUrl: String,
     @SerializedName("title") val title: String,
-    @SerializedName("user") val user: User,
+    @SerializedName("user") val user: UserResponse,
     @SerializedName("body") val body: String,
     @SerializedName("created_at") val createdAt: Date
 )
