d49fa525cedbd73d2346967012161b7655d27a9d
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-08 14:23:25 -0300

    Changing type from SUCCESS to RESULT, because in this architecture proposal, this object will contain the ResultWrapper<SUCCESS, ERROR>

--- domain/src/main/java/br/com/pebmed/domain/usecases/BaseUseCase.kt
+++ domain/src/main/java/br/com/pebmed/domain/usecases/BaseUseCase.kt
@@ -4,14 +4,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
-abstract class BaseUseCase<SUCCESS, PARAMS> {
+abstract class BaseUseCase<RESULT, PARAMS> {
 
-    abstract suspend fun run(params: PARAMS): SUCCESS
+    abstract suspend fun run(params: PARAMS): RESULT
 
     open operator fun invoke(
         scope: CoroutineScope,
         params: PARAMS,
-        onResult: (SUCCESS) -> Unit = {}
+        onResult: (RESULT) -> Unit = {}
     ) {
         val backgroundJob = scope.async { run(params) }
         scope.launch { onResult(backgroundJob.await()) }
