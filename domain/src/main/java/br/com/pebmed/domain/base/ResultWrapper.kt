52282c084e303572c8016c369895634aa81d17ac
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-14 16:14:52 -0300

    Creating new ResultWrapper class hierarchy, to provide more flexibility when handling results (with just Success and Error objects, or with KeyValueMap and StatusCode details)

--- domain/src/main/java/br/com/pebmed/domain/base/ResultWrapper.kt
+++ domain/src/main/java/br/com/pebmed/domain/base/ResultWrapper.kt
@@ -1,100 +1,53 @@
 package br.com.pebmed.domain.base
 
-sealed class ResultWrapper<out SUCCESS, out ERROR>(
-    var keyValueMap: MutableMap<String, String>? = null,
-    var statusCode: StatusType? = StatusType.DEFAULT_EXCEPTION
+open class ResultWrapper<SUCCESS, ERROR>(
+    val success: SUCCESS? = null,
+    val error: ERROR? = null
 ) {
-    class Success<out SUCCESS>(
-        val data: SUCCESS,
-        keyValueMap: MutableMap<String, String>? = null,
-        statusCode: StatusType? = StatusType.OK
-    ) : ResultWrapper<SUCCESS, Nothing>(keyValueMap, statusCode)
 
-    class Error<ERROR>(
-        val error: ERROR? = null,
-        keyValueMap: MutableMap<String, String>? = null,
-        statusCode: StatusType? = StatusType.DEFAULT_EXCEPTION
-    ) : ResultWrapper<Nothing, ERROR>(keyValueMap = keyValueMap, statusCode = statusCode)
+    fun isSuccess() = success != null
 
-    fun getValue(key: String): String? {
-        var result: String? = null
-
-        if (keyValueMap != null) {
-            result = keyValueMap!![key]
-        }
-
-        return result
-    }
-
-
-    fun <NEW_SUCCESS, NEW_ERROR> transform(
+    open fun <NEW_SUCCESS, NEW_ERROR> transform(
         mapperSuccessFunction: (obj: SUCCESS) -> NEW_SUCCESS,
         mapperErrorFunction: (obj: ERROR?) -> NEW_ERROR
     ): ResultWrapper<NEW_SUCCESS, NEW_ERROR> {
-        return when (this) {
-            is Success -> {
-                val newSuccess = mapperSuccessFunction.invoke(this.data)
-                Success(
-                    data = newSuccess,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
-
-            is Error -> {
-                val newError = mapperErrorFunction.invoke(this.error)
-                Error(
-                    error = newError,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
+        return if (this.success != null) {
+            ResultWrapper(
+                success = mapperSuccessFunction.invoke(this.success)
+            )
+        } else {
+            ResultWrapper(
+                error = mapperErrorFunction.invoke(this.error)
+            )
         }
     }
 
-    fun <NEW_SUCCESS> transformSuccess(
+    open fun <NEW_SUCCESS> transformSuccess(
         mapperFunction: (obj: SUCCESS) -> NEW_SUCCESS
     ): ResultWrapper<NEW_SUCCESS, ERROR> {
-        return when (this) {
-            is Success -> {
-                val newSuccess = mapperFunction.invoke(this.data)
-                Success(
-                    data = newSuccess,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
-
-            is Error -> {
-                Error(
-                    error = this.error,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
+        return if (this.success != null) {
+            ResultWrapper(
+                success = mapperFunction.invoke(this.success)
+            )
+        } else {
+            ResultWrapper(
+                error = this.error
+            )
         }
     }
 
-    fun <NEW_ERROR> transformError(
+    open fun <NEW_ERROR> transformError(
         mapperFunction: (obj: ERROR?) -> NEW_ERROR
     ): ResultWrapper<SUCCESS, NEW_ERROR> {
-        return when (this) {
-            is Success -> {
-                Success(
-                    data = this.data,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
-
-            is Error -> {
-                val newError = mapperFunction.invoke(this.error)
-                Error(
-                    error = newError,
-                    keyValueMap = this.keyValueMap,
-                    statusCode = this.statusCode
-                )
-            }
+        return if (this.success != null) {
+            ResultWrapper(
+                success = this.success
+            )
+        } else {
+            ResultWrapper(
+                error = mapperFunction.invoke(this.error)
+            )
         }
     }
 }
+
---
+++ domain/src/main/java/br/com/pebmed/domain/base/SuperResultWrapperV2.kt
@@ -0,0 +1,75 @@
+package br.com.pebmed.domain.base
+
+class SuperResultWrapperV2<SUCCESS, ERROR>(
+    success: SUCCESS? = null,
+    error: ERROR? = null,
+    val keyValueMap: MutableMap<String, String>? = null,
+    val statusCode: StatusType = StatusType.DEFAULT_EXCEPTION
+) : ResultWrapper<SUCCESS, ERROR>(success, error) {
+
+    fun getValue(key: String): String? {
+        var result: String? = null
+
+        if (keyValueMap != null) {
+            result = keyValueMap[key]
+        }
+
+        return result
+    }
+
+    override fun <NEW_SUCCESS, NEW_ERROR> transform(
+        mapperSuccessFunction: (obj: SUCCESS) -> NEW_SUCCESS,
+        mapperErrorFunction: (obj: ERROR?) -> NEW_ERROR
+    ): SuperResultWrapperV2<NEW_SUCCESS, NEW_ERROR> {
+
+        return if (this.success != null) {
+            SuperResultWrapperV2(
+                success = mapperSuccessFunction.invoke(this.success),
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        } else {
+            SuperResultWrapperV2(
+                error = mapperErrorFunction.invoke(this.success),
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        }
+    }
+
+    override fun <NEW_SUCCESS> transformSuccess(
+        mapperFunction: (obj: SUCCESS) -> NEW_SUCCESS
+    ): SuperResultWrapperV2<NEW_SUCCESS, ERROR> {
+        return if (this.success != null) {
+            SuperResultWrapperV2(
+                success = mapperFunction.invoke(this.success),
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        } else {
+            SuperResultWrapperV2(
+                error = this.error,
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        }
+    }
+
+    override fun <NEW_ERROR> transformError(
+        mapperFunction: (obj: ERROR?) -> NEW_ERROR
+    ): SuperResultWrapperV2<SUCCESS, NEW_ERROR> {
+        return if (this.success != null) {
+            SuperResultWrapperV2(
+                success = this.success,
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        } else {
+            SuperResultWrapperV2(
+                error = mapperFunction.invoke(this.success),
+                statusCode = this.statusCode,
+                keyValueMap = this.keyValueMap
+            )
+        }
+    }
+}
