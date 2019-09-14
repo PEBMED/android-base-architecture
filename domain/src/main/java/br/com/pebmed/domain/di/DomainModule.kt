3d350e1a477cfb23c5b9e5477aad11961381a4f5
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-14 20:58:15 -0300

    Separating Domain and UseCase modules

--- domain/src/main/java/br/com/pebmed/domain/di/DomainModule.kt
+++ domain/src/main/java/br/com/pebmed/domain/di/DomainModule.kt
@@ -1,22 +1,5 @@
 package br.com.pebmed.domain.di
 
-import br.com.pebmed.domain.usecases.GetReposUseCase
-import br.com.pebmed.domain.usecases.ListPullRequestsUseCase
-import org.koin.dsl.module
-
-val useCaseModule = module {
-
-    factory {
-        GetReposUseCase(
-            repoRepository = get()
-        )
-    }
-
-    factory {
-        ListPullRequestsUseCase(
-            pullRequestRepository = get()
-        )
-    }
-}
-
-val domainModule = listOf(useCaseModule)
+val domainModule = listOf(
+    useCaseModule
+)
---
+++ domain/src/main/java/br/com/pebmed/domain/di/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package br.com.pebmed.domain.di
+
+import br.com.pebmed.domain.usecases.GetReposUseCase
+import br.com.pebmed.domain.usecases.ListPullRequestsUseCase
+import org.koin.dsl.module
+
+val useCaseModule = module {
+
+    factory {
+        GetReposUseCase(
+            repoRepository = get()
+        )
+    }
+
+    factory {
+        ListPullRequestsUseCase(
+            pullRequestRepository = get()
+        )
+    }
+}
