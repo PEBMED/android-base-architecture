9c584b3464f85cc65763fbb472861178ac20e72c
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-15 01:47:49 -0300

    Externalising callback functions to turn possible to test your calls

--- data/src/main/java/br/com/pebmed/data/repository/PullRequestRepositoryImpl.kt
+++ data/src/main/java/br/com/pebmed/data/repository/PullRequestRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package br.com.pebmed.data.repository
 
+import br.com.pebmed.data.remote.model.response.PullRequestResponse
 import br.com.pebmed.data.remote.source.PullRequestRemoteDataSource
 import br.com.pebmed.domain.base.BaseErrorData
 import br.com.pebmed.domain.base.ResultWrapper
+import br.com.pebmed.domain.entities.GitHubErrorData
 import br.com.pebmed.domain.entities.PullRequest
 import br.com.pebmed.domain.repository.PullRequestRepository
 
@@ -17,17 +19,25 @@ class PullRequestRepositoryImpl(
         val listPullRequests = pullRequestRemoteDataSource.listPullRequests(owner, repoName)
 
         return listPullRequests.transform(
-            mapperSuccessFunction = {
-                it.map { pullRequestResponse ->
-                    pullRequestResponse.mapTo()
-                }
-            },
-            mapperErrorFunction = { baseErrorData ->
-                BaseErrorData(
-                    errorBody = baseErrorData?.errorBody?.message,
-                    errorMessage = baseErrorData?.errorMessage
-                )
-            }
+            mapperSuccessFunction = this.handleListPullRequestsSuccess(),
+            mapperErrorFunction = this.handleListPullRequestsError()
         )
+    }
+
+    private fun handleListPullRequestsSuccess(): (List<PullRequestResponse>) -> List<PullRequest> {
+        return {
+            it.map { pullRequestResponse ->
+                pullRequestResponse.mapTo()
+            }
+        }
+    }
+
+    private fun handleListPullRequestsError(): (BaseErrorData<GitHubErrorData>?) -> BaseErrorData<String> {
+        return { baseErrorData ->
+            BaseErrorData(
+                errorBody = baseErrorData?.errorBody?.message,
+                errorMessage = baseErrorData?.errorMessage
+            )
+        }
     }
 }
