6bd0c953de751f5e73f2a4029c6c2ae06f328810
Luis Fernandez <luis82fernandez@gmail.com>
2019-09-14 22:57:41 -0300

    Simplifying the code, sending "calback" functions directly

--- data/src/main/java/br/com/pebmed/data/repository/PullRequestRepositoryImpl.kt
+++ data/src/main/java/br/com/pebmed/data/repository/PullRequestRepositoryImpl.kt
@@ -1,11 +1,8 @@
 package br.com.pebmed.data.repository
 
-import br.com.pebmed.data.remote.model.response.PullRequestResponse
 import br.com.pebmed.data.remote.source.PullRequestRemoteDataSource
 import br.com.pebmed.domain.base.BaseErrorData
 import br.com.pebmed.domain.base.ResultWrapper
-import br.com.pebmed.domain.entities.GetReposErrorData
-import br.com.pebmed.domain.entities.GitHubErrorData
 import br.com.pebmed.domain.entities.PullRequest
 import br.com.pebmed.domain.repository.PullRequestRepository
 
@@ -19,25 +16,17 @@ class PullRequestRepositoryImpl(
         val listPullRequests = pullRequestRemoteDataSource.listPullRequests(owner, repoName)
 
         return listPullRequests.transform(
-            functionSuccess(),
-            function()
-        )
-    }
-
-    private fun functionSuccess(): (List<PullRequestResponse>) -> List<PullRequest> {
-        return {
-            it.map { pullRequestResponse ->
-                pullRequestResponse.mapTo()
+            {
+                it.map { pullRequestResponse ->
+                    pullRequestResponse.mapTo()
+                }
+            },
+            { baseErrorData ->
+                BaseErrorData(
+                    errorBody = baseErrorData?.errorBody?.message,
+                    errorMessage = baseErrorData?.errorMessage
+                )
             }
-        }
-    }
-
-    private fun function(): (BaseErrorData<GitHubErrorData>?) -> BaseErrorData<String>? {
-        return { baseErrorData ->
-            BaseErrorData(
-                errorBody = baseErrorData?.errorBody?.message,
-                errorMessage = baseErrorData?.errorMessage
-            )
-        }
+        )
     }
 }
