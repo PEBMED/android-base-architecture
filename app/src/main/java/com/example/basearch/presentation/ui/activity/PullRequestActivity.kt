a2e7ebf9215c779294bc8b367fd638652db236ff
Luis Fernandez <luis82fernandez@gmail.com>
2019-11-30 13:23:53 -0300

    Adding new PullRequestActivity

---
+++ app/src/main/java/com/example/basearch/presentation/ui/activity/PullRequestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.basearch.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basearch.R
+
+class PullRequestActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pull_request)
+    }
+}
