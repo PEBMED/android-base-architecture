b5f4bd4d2ca8a359028e6176e0481c7fb1c00cde
Thiago Lacerda <thiago.lacerda@pebmed.com.br>
2020-12-22 18:23:21 -0300

    Refactoring mock returns

--- data/src/test/java/br/com/pebmed/data/pullrequest/model/fake/MockPullRequestResponseModel.kt
+++ data/src/test/java/br/com/pebmed/data/pullrequest/model/fake/MockPullRequestResponseModel.kt
@@ -2,26 +2,68 @@ package br.com.pebmed.data.pullrequest.model.fake
 
 import br.com.pebmed.data.pullRequest.model.PullRequestResponseModel
 import br.com.pebmed.data.pullRequest.model.UserResponseModel
+import br.com.pebmed.domain.MockPullRequestModel
+import br.com.pebmed.domain.MockUserModel
+import io.mockk.every
+import io.mockk.mockk
 import java.util.*
 
 class MockPullRequestResponseModel {
 
     companion object {
+        fun loadPullRequestResponse(userResponseModel: UserResponseModel) : PullRequestResponseModel {
+            val mockedPullRequestResponseModel = mockk<PullRequestResponseModel>()
+            every {
+                mockedPullRequestResponseModel.number
+            } returns 1
 
-        fun loadPullRequestResponse(
-            userResponseModel: UserResponseModel
-        ) = PullRequestResponseModel(
-                number = 1,
-                htmlUrl = "http://the.url",
-                title = "Title",
-                userModel = userResponseModel,
-                body = "Body",
-                createdAt = Date(),
-                comments = 1,
-                commits = 1,
-                additions = 1,
-                deletions = 1,
-                changedFiles = 1
+            every {
+                mockedPullRequestResponseModel.htmlUrl
+            } returns "http://the.url"
+
+            every {
+                mockedPullRequestResponseModel.title
+            } returns "Title"
+
+            every {
+                mockedPullRequestResponseModel.userModel
+            } returns userResponseModel
+
+            every {
+                mockedPullRequestResponseModel.body
+            } returns "Body"
+
+            every {
+                mockedPullRequestResponseModel.createdAt
+            } returns Date()
+
+            every {
+                mockedPullRequestResponseModel.comments
+            } returns 1
+
+            every {
+                mockedPullRequestResponseModel.commits
+            } returns 1
+
+            every {
+                mockedPullRequestResponseModel.additions
+            } returns 1
+
+            every {
+                mockedPullRequestResponseModel.deletions
+            } returns 1
+
+            every {
+                mockedPullRequestResponseModel.changedFiles
+            } returns 1
+
+            every {
+                mockedPullRequestResponseModel.mapTo()
+            } returns MockPullRequestModel.mock(
+                MockUserModel.mock()
             )
+
+            return mockedPullRequestResponseModel
+        }
     }
 }
--- data/src/test/java/br/com/pebmed/data/pullrequest/model/fake/MockUserResponseModel.kt
+++ data/src/test/java/br/com/pebmed/data/pullrequest/model/fake/MockUserResponseModel.kt
@@ -1,12 +1,27 @@
 package br.com.pebmed.data.pullrequest.model.fake
 
 import br.com.pebmed.data.pullRequest.model.UserResponseModel
+import br.com.pebmed.domain.MockUserModel
+import io.mockk.every
+import io.mockk.mockk
 
 class MockUserResponseModel {
     companion object {
-        fun loadUserResponse() = UserResponseModel(
-            login = "luis.fernandez",
-            avatarUrl = "http://avatar.url"
-        )
+        fun loadUserResponse() : UserResponseModel {
+            val mockedUserResponseModel = mockk<UserResponseModel>()
+            every {
+                mockedUserResponseModel.login
+            } returns "luis.fernandez"
+
+            every {
+                mockedUserResponseModel.avatarUrl
+            } returns "http://avatar.url"
+
+            every {
+                mockedUserResponseModel.mapTo()
+            } returns MockUserModel.mock()
+
+            return mockedUserResponseModel
+        }
     }
 }
