bda4f8f10c0a77dc38b7d83f8508fae30e1cf85a
Renan Barbieri <renan.barbieri@pebmed.com.br>
2020-12-22 11:29:53 -0300

    Adding comment to explain SingleLiveEvent

--- platform/src/main/java/com/pebmed/platform/base/SingleLiveEvent.java
+++ platform/src/main/java/com/pebmed/platform/base/SingleLiveEvent.java
@@ -11,13 +11,23 @@ import androidx.lifecycle.Observer;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * A lifecycle-aware observable that sends only new updates after subscription, used for events like
+ * navigation and Snackbar messages.
+ * <p>
+ * This avoids a common problem with events: on configuration change (like rotation) an update
+ * can be emitted if the observer is active. This LiveData only calls the observable if there's an
+ * explicit call to setValue() or call().
+ * <p>
+ * Note that only one observer is going to be notified of changes.
+ */
 public class SingleLiveEvent<T> extends MutableLiveData<T> {
 
     private static final String TAG = "SingleLiveEvent";
 
     private final AtomicBoolean mPending = new AtomicBoolean(false);
 
-    @MainThread
+    @MainThread @Override
     public void observe(@NonNull LifecycleOwner owner, @NonNull final Observer<? super T> observer) {
 
         if (hasActiveObservers()) {
