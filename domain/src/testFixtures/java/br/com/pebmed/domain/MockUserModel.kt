1f3c54a98807e6dfd0d9cd1559355ef15ccc81e2
Thiago Lacerda <thiago.lacerda@pebmed.com.br>
2020-12-23 16:00:18 -0300

    Refactoring mock user model to class object and returning mocked atributes

--- domain/src/testFixtures/java/br/com/pebmed/domain/MockUserModel.kt
+++ domain/src/testFixtures/java/br/com/pebmed/domain/MockUserModel.kt
@@ -1,13 +1,27 @@
 package br.com.pebmed.domain
 
 import br.com.pebmed.domain.entities.UserModel
+/**
+ * https://issuetracker.google.com/issues/139438142
+ * Bug na IDE que não encontra dependências importadas
+ * O build continua funcionando normalmente mesmo com esse problema
+ * gerado ao utilizar o java-test-fixtures plugin
+ */
+import io.mockk.*
 
-class MockUserModel {
-    
-    companion object {
-        fun mock() = UserModel(
-            login = "luis.fernandez",
-            avatarUrl = "http://avatar.url"
-        )
+object MockUserModel {
+
+    fun mock() : UserModel {
+        val mockedUserModel = mockk<UserModel>()
+
+        every {
+            mockedUserModel.login
+        } returns "luis.fernandez"
+
+        every {
+            mockedUserModel.avatarUrl
+        } returns "http://avatar.url"
+
+        return mockedUserModel
     }
 }
