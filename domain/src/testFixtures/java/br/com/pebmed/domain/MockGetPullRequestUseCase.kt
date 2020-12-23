4b6b044f45911b2c6ddfacb4b1793d0fb1e25804
Thiago Lacerda <thiago.lacerda@pebmed.com.br>
2020-12-23 12:04:55 -0300

    Creating request use case mock

---
+++ domain/src/testFixtures/java/br/com/pebmed/domain/MockGetPullRequestUseCase.kt
@@ -0,0 +1,20 @@
+package br.com.pebmed.domain
+
+import br.com.pebmed.domain.base.ResultWrapper
+import br.com.pebmed.domain.usecases.GetPullRequestUseCase
+/**
+ * https://issuetracker.google.com/issues/139438142
+ * Bug na IDE que não encontra dependências importadas
+ * O build continua funcionando normalmente mesmo com esse problema
+ * gerado ao utilizar o java-test-fixtures plugin
+ */
+import io.mockk.*
+
+class MockGetPullRequestUseCase(val mock: GetPullRequestUseCase) {
+
+    fun mockSuccess() {
+        coEvery {
+            mock.runAsync(any())
+        } returns ResultWrapper(success = MockPullRequestModel.mock(MockUserModel.mock()))
+    }
+}
