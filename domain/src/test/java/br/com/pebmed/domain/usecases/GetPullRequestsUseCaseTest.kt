920661b54720912302bc2e8198aa66be5393ed86
renanBarbieri <renan.hbarbieri@gmail.com>
2020-12-29 19:19:17 -0300

    sanitizing GetPullRequestsUseCaseTest

--- domain/src/test/java/br/com/pebmed/domain/usecases/GetPullRequestsUseCaseTest.kt
+++ domain/src/test/java/br/com/pebmed/domain/usecases/GetPullRequestsUseCaseTest.kt
@@ -3,7 +3,9 @@ package br.com.pebmed.domain.usecases
 import br.com.pebmed.domain.MockGetPullRequestListUseCase
 import br.com.pebmed.domain.base.BaseErrorStatus
 import br.com.pebmed.domain.repository.MockPullRequestRepository
-import io.mockk.*
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -27,29 +29,37 @@ class GetPullRequestsUseCaseTest {
      */
     @Test
     fun `SHOULD return pull requests list WHEN success fetched`() = runBlocking {
+        //ARRANGE
         mockPullRequestRepository.mockGetPullRequestsListWithOneItem()
 
+        //ACT
         val resultWrapper = getPullRequestsUseCase.runAsync(MockGetPullRequestListUseCase.mockGenericParams())
 
+        //ASSERT
         assertEquals("luis.fernandez", resultWrapper.success?.get(0)?.user?.login)
     }
 
     @Test
     fun `SHOULD return default error WHEN error fetched`() = runBlocking {
+        //ARRANGE
         mockPullRequestRepository.mockGetPullRequestWithBaseErrorDataUnit()
 
+        //ACT
         val resultWrapper = getPullRequestsUseCase.runAsync(MockGetPullRequestListUseCase.mockGenericParams())
 
+        //ASSERT
         assertEquals(BaseErrorStatus.DEFAULT_ERROR, resultWrapper.error?.errorBody)
     }
 
     @Test
     fun `SHOULD call correct dependency function WHEN run`() = runBlocking {
+        //ARRANGE
         mockPullRequestRepository.mockGetPullRequestsListWithOneItem()
 
-
+        //ACT
         getPullRequestsUseCase.runAsync(MockGetPullRequestListUseCase.mockGenericParams())
 
+        //ASSERT
         coVerify {
             mockPullRequestRepository.mock.getPullRequests(any(), any())
         }
