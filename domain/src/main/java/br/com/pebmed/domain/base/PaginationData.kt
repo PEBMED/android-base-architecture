61a7c056817d9e24ab51da99b11a0714b6a79637
Ruan Vasconcelos <ruan.vasconcelos.2012@gmail.com>
2021-02-26 13:42:54 -0300

    Create doc for PaginationData.kt

--- domain/src/main/java/br/com/pebmed/domain/base/PaginationData.kt
+++ domain/src/main/java/br/com/pebmed/domain/base/PaginationData.kt
@@ -1,5 +1,9 @@
 package br.com.pebmed.domain.base
 
+/**
+ * @Descrição: PaginationData armazena as informações de paginação de determinado conteudo e também
+ * possui as diretrizes de primeira página e limite de conteúdos por página.
+ */
 data class PaginationData(
         val nextPage: Int,
         val hasNextPage: Boolean
